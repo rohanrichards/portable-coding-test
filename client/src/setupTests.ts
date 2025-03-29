4e24ad88184e29fa07f4f02d544bfcbf5723db9b
Rohan Richards <rohan.m.richards@gmail.com>
2025-03-30 09:17:19 +1000

    fix error with testing

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -2,4 +2,12 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom'; 
+import '@testing-library/jest-dom';
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toBeInTheDocument(): R;
+    }
+  }
+} 
