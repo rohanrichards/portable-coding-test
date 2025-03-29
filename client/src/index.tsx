78bdbae209823ed16eb7da341972596439638b1d
Rohan Richards <rohan.m.richards@gmail.com>
2025-03-30 08:51:31 +1000

    typescript in the client
    migrate the client to typescript
    fix up some package versions
    fix some issues with the package upgrade

---
+++ client/src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Optional: Start measuring performance
+reportWebVitals(); 
