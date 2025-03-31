94d98815e2a42850ff88a22d4e94a69e355ccbb6
Rohan Richards <rohan.m.richards@gmail.com>
2025-03-31 10:12:01 +1000

    implement multiple providers (hardcoded for now) as part of the 3rd interview

--- server/src/api/search.ts
+++ server/src/api/search.ts
@@ -1,19 +1,50 @@
 import express, { Request, Response, Router } from 'express';
 const router: Router = express.Router();
-const guardianUrl = 'https://content.guardianapis.com/search?'
-const buildGuardianQuery = (term: string): URL => {
-  const url = new URL(guardianUrl)
-  url.searchParams.append('api-key', process.env.GUARDIAN_API_KEY ?? '')
-  url.searchParams.append('q', term)
-  return url;
+
+const providers: any = {
+  "dummy": {
+    baseUrl: 'http://localhost/dummy',
+    queryBuilder: (term: string, baseUrl: string): URL => {
+      const url = new URL(baseUrl)
+      url.searchParams.append('api-key', process.env.GUARDIAN_API_KEY ?? '')
+      url.searchParams.append('q', term)
+      return url;
+    },
+    getResults: async (url: URL) => {
+      // const providerResult = await fetch(url)
+      // const data = await providerResult.json() as any;
+      return {provider: "dummy", results: [{webTitle: "Dummy Article"}]};
+    }
+  },
+  "guardian": {
+    baseUrl: 'https://content.guardianapis.com/search?',
+    queryBuilder: (term: string, baseUrl: string): URL => {
+      const url = new URL(baseUrl)
+      url.searchParams.append('api-key', process.env.GUARDIAN_API_KEY ?? '')
+      url.searchParams.append('q', term)
+      return url;
+    },
+    getResults: async (url: URL) => {
+      const providerResult = await fetch(url)
+      const data = await providerResult.json() as any;
+      console.log(data)
+      return {provider: "guardian", results: data.response.results};
+    }
+  }
 }
 
 router.get('/', async (req: Request, res: Response) => {
   const searchTerm = req.query.term as string;
-  const guardianResult = await fetch(buildGuardianQuery(searchTerm))
-  const data = await guardianResult.json() as any
-  console.log(data)
-  res.json({results: data.response.results});
+  // const searchProviders = (req.query.providers as string).split(',');
+  const searchProviders = ['guardian', 'dummy'];
+  const queries = searchProviders.map(provider => {
+    const query = providers[provider].queryBuilder(searchTerm, providers[provider].baseUrl);
+    const promise = () => providers[provider].getResults(query)
+    return {query, promise};
+  })
+  const allProviderData = await Promise.all(queries.map(q => q.promise()))
+  console.log(allProviderData)
+  res.json({results: allProviderData});
 });
 
 export default router; 
